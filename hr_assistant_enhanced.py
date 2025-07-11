4d04c65295a5bed6968b84a112d8e4c6a282ac97
Ahmed Abdelhedi <ahmedabdelhedi899@gmail.com>
2025-07-11 15:42:55 +0100

    final-commit

--- hr_assistant_enhanced.py
+++ hr_assistant_enhanced.py
@@ -27,6 +27,7 @@ if "groq_api_key" not in st.session_state:
         try:
             with open("api.txt", "r", encoding="utf-8") as f:
                 st.session_state.groq_api_key = f.read().strip()
+                print('')
                 if st.session_state.groq_api_key:
                     st.success("✅ Clé API Groq chargée depuis api.txt")
         except FileNotFoundError: 
